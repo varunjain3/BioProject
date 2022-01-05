5f883a19785b8e65de2e6205abc5d1e110bc2d69
Varun Jain <varun.jain@iitgn.ac.in>
2022-01-05 23:46:20 +0530

    added lnc2rna filled

---
+++ data/lnc2rna_filled_data/filldata.py
@@ -0,0 +1,79 @@
+import pandas as pd
+from tqdm import tqdm
+
+# mapping between orignal_data and lnc2rna_data
+mapping = {
+    "1. Database-LncRNAs-Colorectal Cancer.xlsx": "Colorectal.csv",
+    "2. Database-LncRNAs-Ovarian Cancer.xlsx": "Ovarian.csv",
+    "3. Database-LncRNAs-Pancreatic Cancer.xlsx": "Pancreatic.csv",
+    "4. Database-LncRNAs-Cervical Cancer.xlsx": "Cervical.csv",
+    "5. Database-LncRNAs-Gastric Cancer.xlsx": "Gastric.csv",
+    "6. Database-LncRNAs-Head and neck Cancer.xlsx": "Head and neck.csv",
+    "7. Database-LncRNAs-Lung Cancer.xlsx": "Lung.csv",
+    "8. Database-LncRNAs-Liver Cancer.xlsx": "Liver.csv",
+    "9. Database-LncRNAs-Prostate Cancer.xlsx": "Prostate.csv",
+}
+
+
+for key, value in mapping.items():
+        
+    database = pd.read_excel(f"./data/orignal_data/{key}",sheet_name="Validated & Reviewed GQ LncRNAs",header=1)
+
+    search = pd.read_csv("./data/lnc2rna_data/{}".format(value))
+
+    new = pd.DataFrame(columns=database.columns)
+
+    fail = 0 
+    success = 0
+    filled = 0
+    for i, row in tqdm(database.iterrows()):
+        
+        flag = 0
+        if pd.isnull(row).all():
+            continue
+        
+        if pd.isnull(row["Cancer name"]):
+            flag = 1
+        if pd.isnull(row["Methods"]):
+            flag = 1
+        if pd.isnull(row["Expression pattern"]):
+            flag = 1
+        if pd.isnull(row["Pubmed ID"]):
+            flag = 1
+        
+        if flag:
+            data = search[search["Lnc/ CircRNA name"] == row["LncRNA name"]]
+            if pd.isnull(row["LncRNA name"]) or isinstance(row["LncRNA name"],int):
+                new = new.append(row)
+                continue
+
+            if len(data) == 0:
+                if len(row["LncRNA name"].split()) > 1:
+                    data = search[search["Lnc/ CircRNA name"] == row["LncRNA name"].split()[0]]
+                    if len(data) == 0:
+                        fail += 1
+                        new = new.append(row)
+                else:
+                    fail += 1
+                    new = new.append(row)
+                
+
+            
+            for i, search_data in data.iterrows():
+                row["Cancer name"] = search_data["Cancer name"]
+                row["Methods"] = search_data["Methods"]
+                row["Expression pattern"] = search_data["Expression pattern"]
+                row["Pubmed ID"] = search_data["Pubmed ID"]
+                new = new.append(row)
+                success += 1
+            
+            
+        else:
+            filled += 1
+            new = new.append(row)
+
+
+    print("For file {}: {} success, {} fail, {} already filled".format(key, success, fail, filled))
+    new.to_excel("./data/lnc2rna_filled_data/{}".format(value.split()[0]+".xlsx"),sheet_name="Validated & Reviewed GQ LncRNAs",header=1,index=False)
+
+
